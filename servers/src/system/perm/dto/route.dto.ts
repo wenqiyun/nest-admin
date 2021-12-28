4a707508193dd7da6dabb92d9ac522884bbba33a
wenqiyun <weqiyun@gmail.com>
2021-12-28 22:53:43 +0800

    route.dto  method 方法枚举

--- servers/src/system/perm/dto/route.dto.ts
+++ servers/src/system/perm/dto/route.dto.ts
@@ -1,12 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger'
 
+import { RouterMethods } from '../../../common/enums/routerMethod.enum';
+
 export class RouteDto {
   /** 路由 path */
   @ApiProperty({ description: 'api 路径' })
   path: string
   /** 路由方法 */
-  @ApiProperty({ description: 'api 方法' })
-  method: 'GET' | 'POST' | 'PUT' | 'DELETE'
+  @ApiProperty({ description: 'api 方法', enum: RouterMethods })
+  method: RouterMethods
   /** 路由描述 */
   @ApiProperty({ description: 'api 描述说明', required: false })
   desc?: string
