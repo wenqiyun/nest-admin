70552ea596243da874442ddc9a09f55686d6c4f8
wenqiyun <weqiyun@gmail.com>
2023-01-09 14:48:51 +0800

    dto 得加上检验，修复接口报 【an unknown value was passed to the validate function】 如果有相同的错误，按照这上面改

--- servers/src/system/oss/dto/find-oss.dto.ts
+++ servers/src/system/oss/dto/find-oss.dto.ts
@@ -1,11 +1,16 @@
 import { ApiProperty } from '@nestjs/swagger'
+import { IsDateString, IsOptional } from 'class-validator';
 
 import { ReqListQuery } from '../../../common/utils/req-list-query'
 
 export class FindOssDto extends ReqListQuery {
   @ApiProperty({ description: '搜索条件，起始时间', required: false })
+  @IsDateString()
+  @IsOptional()
   startDay?: string
 
   @ApiProperty({ description: '搜索条件，结束时间', required: false })
+  @IsDateString()
+  @IsOptional()
   endDay?: string
 }
